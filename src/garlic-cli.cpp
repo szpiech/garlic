638a1cb3592f2187ab1cc70f5cccb0bf1549f5e0
Zachary Szpiech <szpiech@gmail.com>
2017-05-16 11:32:28 -0700

    v1.1.4

--- src/garlic-cli.cpp
+++ src/garlic-cli.cpp
@@ -1,7 +1,7 @@
 #include "garlic-cli.h"
 #include <iostream>
 
-const string VERSION = "1.1.3";
+const string VERSION = "1.1.4";
 
 const string PREAMBLE = "\ngarlic v" + VERSION + " -- a program to call runs of homozygosity in genetic data.\n\
 Source code and binaries can be found at <https://www.github.com/szpiech/garlic>.\n\
